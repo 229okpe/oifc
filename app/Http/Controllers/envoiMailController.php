f85c0f807ab12b9e6a543614494abce4a6e7d3ff
229Okpe <akoffodjic@gmail.com>
2022-04-26 18:33:36 +0100

    11

--- app/Http/Controllers/envoiMailController.php
+++ app/Http/Controllers/envoiMailController.php
@@ -31,7 +31,13 @@ class envoiMailController extends Controller
        'message' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque feugiat est eu sapien lacinia interdum. Sed vitae nibh urna. Etiam quis lorem ullamcorper, ullamcorper justo non, feugiat nunc. Etiam nulla quam, maximus id blandit at, sodales at quam. Nullam interdum arcu ac lacinia suscipit. Donec a purus nisl.'
    ];*/
 
-        Mail::to('test@mail.test')->send( new mailContact($formContactData) );
-        dd('Mail envoyé');
+        if(Mail::to('test@mail.test')->send( new mailContact($formContactData) )){
+            return response()->json([
+                'message' => 'envoyé',
+                
+            ]);
+        }
+        }
+
     }
 }
