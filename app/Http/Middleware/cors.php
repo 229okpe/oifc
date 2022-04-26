d84a5e1e9c84ef9688b7a5acf07fdb0c1abb2721
229Okpe <akoffodjic@gmail.com>
2022-04-26 21:31:34 +0100

    l

--- app/Http/Middleware/cors.php
+++ app/Http/Middleware/cors.php
@@ -17,7 +17,7 @@ class cors
     public function handle($request, Closure $next)
 {
     return $next($request)->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods','GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+                            ->header('Access-Control-Allow-Methods','GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS')
+                            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
 }
 }
