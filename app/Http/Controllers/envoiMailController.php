55bb1a563c4d34dc556f15644ddf6f616711d208
229Okpe <akoffodjic@gmail.com>
2022-04-26 18:27:30 +0100

    10

--- app/Http/Controllers/envoiMailController.php
+++ app/Http/Controllers/envoiMailController.php
@@ -9,13 +9,27 @@ use Illuminate\Support\Facades\Mail;
 class envoiMailController extends Controller
 {
 
-    public function envoiMailContact(){
-   $formContactData= [
+    public function envoiMailContact($request){
+        $request->validation([
+            'nom'=>['required'],
+            'prenom'=>['required'],
+            'email'=>['required', 'email'],
+            'message'=>['required'],
+        ]);
+
+   
+   $formDevisData= [
+       'nom' => $request->nom,
+       'prenom' =>$request->prenom,
+       'email' => $request->email,
+        'message' => $request->message
+         ];
+ /*  $formContactData= [
        'nom' => 'Cedric Magloire',
        'email' => 'akoffodji@gmail.com',
        'sujet' => 'Salutation',
        'message' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque feugiat est eu sapien lacinia interdum. Sed vitae nibh urna. Etiam quis lorem ullamcorper, ullamcorper justo non, feugiat nunc. Etiam nulla quam, maximus id blandit at, sodales at quam. Nullam interdum arcu ac lacinia suscipit. Donec a purus nisl.'
-   ];
+   ];*/
 
         Mail::to('test@mail.test')->send( new mailContact($formContactData) );
         dd('Mail envoyé');
