8614296e3d38117078a02b3c690652528ee39a03
229Okpe <akoffodjic@gmail.com>
2022-04-26 21:53:27 +0100

    a11

--- routes/api.php
+++ routes/api.php
@@ -12,10 +12,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
-header('Access-Control-Allow-Origin: *');
+*/ 
 Route::middleware('Cors')->group( function() {
 
         Route::get('/interesse/{id}', 'App\Http\Controllers\adhesionCoursController@adhesionCours');
